e2baf44fd20c30fc80b005e3081f23f27e34c76f
Benjamin Paige <benpaigeiii@hotmail.com>
2020-09-06 17:42:23 -0400

    Reorganize entry point
    Before starting a major refactor, I wanted to play around with deobfuscating the application construction.wq

--- letgodbetrue/src/main/java/com/tertioptus/LetGodBeTrueApplication.java
+++ letgodbetrue/src/main/java/com/tertioptus/LetGodBeTrueApplication.java
@@ -6,42 +6,37 @@ import com.tertioptus.rss.PropertiesChannelInformationReceptionist;
 import com.tertioptus.rss.Raven;
 import com.tertioptus.rss.SermonRaven;
 import com.tertioptus.web.SecretarySquirrel;
+import com.tertioptus.web.Squirrel;
 import com.tertioptus.web.WebContentSquirrel;
 import com.tertioptus.web.url.ApacheUrlValidatorEngineer;
 import com.tertioptus.web.url.PropertiesUrlReceptionist;
 import com.tertioptus.web.url.QualifyingUrlReceptionist;
 
 public class LetGodBeTrueApplication implements Application {
-	
-	private static final PropertiesMapEngineer thePropertiesMapEngineer  = 
-			new PropertiesMapEngineer("config.properties", new PropertiesResourceStreamEngineer());
-	
+
+	private final Raven raven;
+	private final MapEngineer<String, String> propertiesMapEngineer;
+	private final static PropertiesMapEngineer thePropertiesMapEngineer = new PropertiesMapEngineer("config.properties",
+			new PropertiesResourceStreamEngineer());
+
 	public LetGodBeTrueApplication() {
-		this(new SermonRaven(
-        		new SecretarySquirrel(
-        				new WebContentSquirrel(
-                   			new QualifyingUrlReceptionist(
-                   					new ApacheUrlValidatorEngineer(), 
-                   					new PropertiesUrlReceptionist(
-                   						thePropertiesMapEngineer	
-                   					)
-                   			)
-                   		)
-                ),
-        		new PropertiesChannelInformationReceptionist(thePropertiesMapEngineer)
-       	)
-		,thePropertiesMapEngineer);
+		this(	
+				new SermonRaven(scrapeySquirrel(), new PropertiesChannelInformationReceptionist(thePropertiesMapEngineer)),
+				thePropertiesMapEngineer
+			);
 	}
 
-	public LetGodBeTrueApplication(Raven raven, MapEngineer<String,String> propertiesMapEngineer) {
+	public LetGodBeTrueApplication(Raven raven, MapEngineer<String, String> propertiesMapEngineer) {
 		this.raven = raven;
 		this.propertiesMapEngineer = propertiesMapEngineer;
 	}
-	
-	private final Raven raven;
-	private final MapEngineer<String, String> propertiesMapEngineer;
 
-	public void run(String[] args) throws Exception{
+	public void run(String[] args) throws Exception {
 		raven.dispatch(propertiesMapEngineer.value("filename"));
+	}
+
+	private static Squirrel scrapeySquirrel() {
+		return new SecretarySquirrel(new WebContentSquirrel(new QualifyingUrlReceptionist(
+				new ApacheUrlValidatorEngineer(), new PropertiesUrlReceptionist(thePropertiesMapEngineer))));
 	}
 }
